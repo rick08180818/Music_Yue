3020f65cf70b914fb9d4f3080efb4bad74cea6ea
rick08180818 <rick08180818@gmail.com>
2023-07-14 22:35:29 +0800

    bug report be better

--- commands/play.js
+++ commands/play.js
@@ -6,12 +6,13 @@ module.exports = {
             .setName('play')
             .setDescription('Plays a song from the playlist'),
     async execute(interaction) {
-        await interaction.deferReply();
+        await interaction.deferReply()
         try {
-            await playNextSong(interaction);
-            await interaction.followUp('Playing the next song from the playlist');
+            await playNextSong(interaction)
+            await interaction.followUp('Playing the next song from the playlist')
         } catch (error) {
-            console.error(`Error in play command: ${error.message}`);
+            console.error(`Error in play command: ${error.message}`)
+            interaction.reply('The playlist is empty,please use /add to add song')
         }
     }
 };
